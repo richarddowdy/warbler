063d838a735a4a99c8b8d44e3a3a2119d0a2831f
moogiecodes <mchoichoi@gmail.com>
2020-02-05 11:34:22 -0800

    Implement logout, fix: user profile, user cards

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -114,6 +114,9 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
+    do_logout()
+    flash("You've logged out successfully!", "success")
+    return redirect('/login')
 
 
 ##############################################################################
